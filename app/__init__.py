694c0c4105c951a28a67dd6665c473fefc55b581
David Asaf <dasaf@atd-us.com>
2019-11-05 01:50:35 -0500

    clean up

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,29 @@
+"Main Flask App"
+# pylint: disable=import-outside-toplevel
+from logging import Logger
+
 from flask import Flask
 
 
 def create_app(config_name):
+    """Create the Flask application
+    
+    Args:
+        config_name (Config): A Config class for the Flask application to use
+    
+    Returns:
+        [Flask]: Flask Application
+    """
     from app.config import config_by_name
 
     app = Flask(__name__)
+    app.logger: Logger
     app.logger.debug(f"CONFIG NAME: {config_name}")
     config = config_by_name[config_name]
     app.config.from_object(config)
 
     @app.route("/")
-    def hello_world():
+    def hello_world() -> str:  # pylint: disable=unused-variable
         return "Hello World!"
 
     return app
--- tasks.py
+++ tasks.py
@@ -1,3 +1,4 @@
+"tasks to be invoked via Invoke python"
 from invoke import task
 
 
@@ -6,7 +7,7 @@ def start(ctx, wsgi_server=False, config_name="dev", host="127.0.0.1"):
     """Start the backend as a dev server or production ready gunicorn server"""
     if wsgi_server:
         ctx.run(
-            f"""gunicorn --bind 0.0.0.0:5000 --workers 2 "app:create_app('{config_name}')" """,
+            f"""gunicorn --bind {host}:5000 --workers 2 "app:create_app('{config_name}')" """,
             pty=True,
             echo=True,
         )
@@ -14,6 +15,16 @@ def start(ctx, wsgi_server=False, config_name="dev", host="127.0.0.1"):
 
     ctx.run(
         f"""export FLASK_ENV=development && export FLASK_APP="app:create_app('{config_name}')" && flask run --host={host}""",
+        pty=True,
+        echo=True,
+    )
+
+
+@task
+def save_dependencies(ctx):
+    """Dump dependencies as config files"""
+    ctx.run(
+        "pip freeze > requirements.txt && conda env export > environment.yml",
         pty=True,
         echo=True,
     )
